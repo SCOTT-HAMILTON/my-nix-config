daa963381b039ae768abb520b952af1a0ad31b73
SCOTT-HAMILTON <sgn.hamilton@protonmail.com>
2020-05-31 13:38:50 +0200

    Update home.nix

--- home.nix
+++ home.nix
@@ -1,6 +1,11 @@
- 
+
 { pkgs ? import <nixpkgs>{}
-, ... }:
+, lib ? pkgs.lib 
+, config
+, options
+, modulesPath
+
+}:
 
 with builtins;
 with lib;
@@ -19,7 +24,7 @@ let
   #   url = "https://github.com/SCOTT-HAMILTON/nur-packages-template/archive/master.tar.gz";
   #   sha256 = "0a7lcizi9sw6jfpdqzjy1fgl50jipjcyglw1s0fad82p43yjwjwg" ;
   # }) {pkgs = pkgs;};
-in
+in rec
 {
   imports = [
 #    nur-no-pkgs.repos.shamilton.modules.home-manager.day-night-plasma-wallpapers 
@@ -28,12 +33,16 @@ in
     ./modules/myvim
     ./modules/redshift-auto
   ];
-  
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.packageOverrides = pkgs: {
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-      inherit pkgs;
-    };
+
+  home.homeDirectory = "/home/scott";
+
+  xdg = {
+    enable = true;
+    cacheHome = "${home.homeDirectory}/.local/cache";
+    configHome = "${home.homeDirectory}/.config";
+    dataHome = "${home.homeDirectory}/.local/share";
+    mime.enable = true;
+    mimeApps.enable = true;
   };
 
   home.packages = [
@@ -73,21 +82,49 @@ in
     xdotool
   ];
 
-  # Inline modules
-  ## Generates the shop file
-  
-
-
-#  services.day-night-plasma-wallpapers.enable = true;
-#  services.day-night-plasma-wallpapers.onCalendar = "*-*-* 16:00:00";
-  services.redshift-auto.enable = true;
-  services.redshift-auto.onCalendar = "*-*-* 16:00:00";
 
   myvim.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+  };
 
   programs.git = {
     enable = true;
     userName  = "SCOTT-HAMILTON";
     userEmail = "sgn.hamilton@protonmail.com";
   };
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    autocd = true;
+    dotDir = ".config/zsh";
+    history = {
+      extended = true;
+      ignoreDups = true;
+      path = "${xdg.dataHome}/zsh/zsh_history";
+    };
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+    };
+    plugins = [
+    {
+      name = "zsh-syntax-highlighting";
+      src = pkgs.fetchFromGitHub {
+        owner = "zsh-users";
+        repo = "zsh-syntax-highlighting";
+        rev = "0.7.1";
+        sha256 = "03r6hpb5fy4yaakqm3lbf4xcvd408r44jgpv4lnzl9asp4sb9qc0";
+      };
+    }];
+  };
+
+  services.redshift-auto.enable = true;
+  services.redshift-auto.onCalendar = "*-*-* 16:00:00";
 }
